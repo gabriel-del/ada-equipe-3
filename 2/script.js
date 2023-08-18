e8ba88ae4c985dd3db9b6c85103f51229a147592
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-08-18 17:21:18 -0300

    eslint fix

--- 2/script.js
+++ 2/script.js
@@ -3,25 +3,25 @@ const listKey = 'todo-list',
   add = document.querySelector('#add'),
   update = document.querySelector('#update'),
   listOut = JSON.parse(localStorage.getItem(listKey) || '[]'),
-  display = ["none", "inline-block"]
-  
-F = f => index => {
-  f(index)
-  updateList()
-}
+  display = ['none', 'inline-block']
+let updateList
+
+const F = f => index => {f(index); updateList()}
 (updateList = () => {
-  localStorage.setItem(listKey, JSON.stringify(listOut)) 
+  localStorage.setItem(listKey, JSON.stringify(listOut))
   const listIn = document.querySelector('ul')
   listIn.innerHTML = ''
-  listOut.forEach((value, index) => {listIn.innerHTML += `
+  listOut.forEach((value, index) => {
+    listIn.innerHTML += `
   <li><span class='${value.done === true ? 'done' : ''}'>${value.Task}</span><div>
   <button id='edit' onclick='edit(${index})' class="bi bi-pencil"/>
   <button id='remove' onclick='F(remove)(${index})' class="bi bi-trash3"/>
   <button id='done' onclick='F(done)(${index})' class="bi bi-bag-check"/>
-  </div></li><hr>`})
+  </div></li><hr>`
+  })
 })()
 input.addEventListener('keypress', e => {
-  if (e.key === 'Enter')  document.querySelector('section').querySelector('button:not([style*="display: none"])').click()
+  if (e.key === 'Enter') document.querySelector('section').querySelector('button:not([style*="display: none"])').click()
 })
 add.addEventListener('click', F(() => {
   if (!input.value) {
@@ -33,17 +33,20 @@ add.addEventListener('click', F(() => {
     input.value = ''
   }
 }))
-showBtnAdd = show => {add.style.display = display[+show];  update.style.display = display[+!show]}
-remove = index => listOut.splice(index, 1)
-done = index => listOut[index].done = !listOut[index].done
-edit = index => {
-  input.value = listOut[index].Task
-  showBtnAdd(false)
-  update.addEventListener('click', () => {
-    listOut[index].Task = input.value
-    input.value = ''
-    showBtnAdd(true)
-    updateList()
-  }, {once: true})
-}
 
+const
+  showBtnAdd = show => {
+    add.style.display = display[+show]; update.style.display = display[+!show]
+  },
+  remove = index => listOut.splice(index, 1),
+  done = index => listOut[index].done = !listOut[index].done,
+  edit = index => {
+    input.value = listOut[index].Task
+    showBtnAdd(false)
+    update.addEventListener('click', () => {
+      listOut[index].Task = input.value
+      input.value = ''
+      showBtnAdd(true)
+      updateList()
+    }, {once: true})
+  }
