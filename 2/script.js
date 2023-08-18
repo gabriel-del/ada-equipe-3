c8cf0aed86c1153ef1126fbdd1a5598bda538f7d
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-08-18 14:53:05 -0300

    create F function

--- 2/script.js
+++ 2/script.js
@@ -3,41 +3,9 @@ const listKey = 'todo-list',
   add = document.querySelector('#add'),
   update = document.querySelector('#update'),
   listOut = JSON.parse(localStorage.getItem(listKey) || '[]')
-
-input.addEventListener('keypress', e => {
-  if (e.key === 'Enter')  add.style.display !== 'none' ? add.click() : update.click()
-})
-add.addEventListener('click', () => {
-  if (!input.value) {
-    input.style.border = '1px solid red'
-    alert('Digite um item para incluir na lista.')
-  } else {
-    input.style.border = ''
-    listOut.push({Task: input.value, done: false})
-    input.value = ''
-    updateList()
-  }
-})
-showBtnAdd = show => {
-if(show){add.style.display = 'block';  update.style.display = 'none'} 
-else {add.style.display = 'none'; update.style.display = 'block'}
-}
-edit = index => {
-  input.value = listOut[index].Task
-  showBtnAdd(false)
-  update.addEventListener('click', () => {
-    listOut[index].Task = input.value
-    input.value = ''
-    updateList()
-    showBtnAdd(true)
-  })
-}
-remove = index => {
-  listOut.splice(index, 1)
-  updateList()
-}
-done = index => {
-  listOut[index].done = !listOut[index].done
+  
+F = f => index => {
+    f(index)
   updateList()
 }
 (updateList = () => {
@@ -45,9 +13,36 @@ done = index => {
   const listIn = document.querySelector('ul')
   listIn.innerHTML = ''
   listOut.forEach((value, index) => {listIn.innerHTML += `
-    <li><span class='${value.done === true ? 'done' : ''}'>${value.Task}</span><div>
-      <button id='edit' onclick='edit("${index}")' class="bi bi-pencil"/>
-      <button id='remove' onclick='remove("${index}")' class="bi bi-trash3"/>
-      <button id='done' onclick='done("${index}")' class="bi bi-bag-check"/>
-    </div></li><hr>`})
+  <li><span class='${value.done === true ? 'done' : ''}'>${value.Task}</span><div>
+  <button id='edit' onclick='edit(${index})' class="bi bi-pencil"/>
+  <button id='remove' onclick='F(remove)(${index})' class="bi bi-trash3"/>
+  <button id='done' onclick='F(done)(${index})' class="bi bi-bag-check"/>
+  </div></li><hr>`})
 })()
+input.addEventListener('keypress', e => {
+  if (e.key === 'Enter')  add.style.display !== 'none' ? add.click() : update.click()
+})
+add.addEventListener('click', F(() => {
+  if (!input.value) {
+    input.style.border = '1px solid red'
+    alert('Digite um item para incluir na lista.')
+  } else {
+    input.style.border = ''
+    listOut.push({Task: input.value, done: false})
+    input.value = ''
+  }
+}))
+showBtnAdd = show => {if(show){add.style.display = 'block';  update.style.display = 'none'} else {add.style.display = 'none'; update.style.display = 'block'}}
+remove = index => listOut.splice(index, 1)
+done = index => listOut[index].done = !listOut[index].done
+edit = index => {
+  input.value = listOut[index].Task
+  showBtnAdd(false)
+  update.addEventListener('click', F(() => {
+    listOut[index].Task = input.value
+    input.value = ''
+    showBtnAdd(true)
+  }))
+}
+
+
