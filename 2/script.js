8c2c9fd78e1eaeec163d51fe4d22015f1b82787b
Lucas Aquino <lucasaquinoq@gmail.com>
2023-08-18 23:05:18 -0300

    Adicionando a funcionalidade de filtrar as tarefas

--- 2/script.js
+++ 2/script.js
@@ -4,8 +4,8 @@ const listKey = 'todo-list',
   update = document.querySelector('#update'),
   listOut = JSON.parse(localStorage.getItem(listKey) || '[]'),
   display = ['none', 'inline-block'],
-  showBtnAdd = show => {add.style.display = display[+show]; update.style.display = display[+!show]},
-  F = f => index => {f(index); updateList()},
+  showBtnAdd = show => { add.style.display = display[+show]; update.style.display = display[+!show] },
+  F = f => index => { f(index); updateList() },
   remove = index => listOut.splice(index, 1),
   done = index => listOut[index].done = !listOut[index].done
 input.addEventListener('keypress', e => {
@@ -17,7 +17,7 @@ add.addEventListener('click', F(() => {
     alert('Digite um item para incluir na lista.')
   } else {
     input.style.border = ''
-    listOut.unshift({Task: input.value, done: false})
+    listOut.unshift({ Task: input.value, done: false })
     input.value = ''
   }
 }))
@@ -29,7 +29,7 @@ function edit(index) {
     input.value = ''
     showBtnAdd(true)
     updateList()
-  }, {once: true})
+  }, { once: true })
 }
 function updateList() {
   localStorage.setItem(listKey, JSON.stringify(listOut))
@@ -44,4 +44,57 @@ function updateList() {
     </div></li><hr>`
   })
 }
+
+function filter() {
+  localStorage.setItem(listKey, JSON.stringify(listOut))
+  const listIn = document.querySelector('ul')
+  listIn.innerHTML = ''
+  listOut.forEach((value, index) => {
+    listIn.innerHTML += `
+    <li><span class='${value.done === true ? 'done' : ''}'>${value.Task}</span><div>
+    <button id='edit' onclick='edit(${index})' class="bi bi-pencil"/>
+    <button id='remove' onclick='F(remove)(${index})' class="bi bi-trash3"/>
+    <button id='done' onclick='F(done)(${index})' class="bi bi-bag-check"/>
+    </div></li><hr>`
+  })
+}
+
+const filterElement = document.querySelector('main .buscador')
+filterElement.addEventListener('input', filterCards)
+
+function filterCards() {
+
+  if (filterElement.value != '') {
+    let filterText = filterElement.value.toLowerCase()
+    localStorage.setItem(listKey, JSON.stringify(listOut))
+    const listIn = document.querySelector('ul')
+    listIn.innerHTML = ''
+    listOut.forEach((value, index) => {
+
+      if (value.Task.toLowerCase().includes(filterText)){
+        listIn.innerHTML += `
+          <li><span class='${value.done === true ? 'done' : ''}'>${value.Task}</span><div>
+          <button id='edit' onclick='edit(${index})' class="bi bi-pencil"/>
+          <button id='remove' onclick='F(remove)(${index})' class="bi bi-trash3"/>
+          <button id='done' onclick='F(done)(${index})' class="bi bi-bag-check"/>
+          </div></li><hr>`
+      }
+    })
+    
+  } else {
+    localStorage.setItem(listKey, JSON.stringify(listOut))
+    const listIn = document.querySelector('ul')
+    listIn.innerHTML = ''
+    listOut.forEach((value, index) => {
+      listIn.innerHTML += `
+        <li><span class='${value.done === true ? 'done' : ''}'>${value.Task}</span><div>
+        <button id='edit' onclick='edit(${index})' class="bi bi-pencil"/>
+        <button id='remove' onclick='F(remove)(${index})' class="bi bi-trash3"/>
+        <button id='done' onclick='F(done)(${index})' class="bi bi-bag-check"/>
+        </div></li><hr>`
+    })
+  }
+}
+
+
 updateList()
