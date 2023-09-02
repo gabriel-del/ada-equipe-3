4f0175609ff92fa0f12d99f7eb19c0678a453bce
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-09-02 15:29:58 -0300

    new Board

--- 3/script.js
+++ 3/script.js
@@ -1,91 +1,44 @@
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
 class Snake {
-    snakeboard = document.getElementById("snakeboard");
-    snakeboard_ctx = snakeboard.getContext("2d");
-    snake = [
-        { x: 200, y: 200 },
-        { x: 190, y: 200 },
-        { x: 180, y: 200 },
-        { x: 170, y: 200 },
-        { x: 160, y: 200 }
-      ]
+    dx = 10; 
+    dy = 0;
+    snake = [{ x: 200, y: 200 }, { x: 190, y: 200 }, { x: 180, y: 200 }, { x: 170, y: 200 }, { x: 160, y: 200 } ]
     constructor(){}
 
-    drawSnake() {
+    static drawSnake() {
         this.snake.forEach(snakePart => {
-            snakeboard_ctx.fillStyle = 'lightblue';
-            snakeboard_ctx.strokeStyle = 'darkblue';
-            snakeboard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
-            snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
-        });
-    }
+            ctx.fillStyle = 'lightblue';
+            ctx.strokeStyle = 'darkblue';
+            ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+            ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+        })}
     
-    move_snake() {
-        // Create the new Snake's head
-        const head = { x: snake[0].x + dx, y: snake[0].y + dy };
-        // Add the new head to the beginning of snake body
-        this.snake.unshift(head);  
+    static move_snake() {
+        const head = { x: this.snake[0].x + this.dx, y: this.snake[0].y + this.dy };
+        this.snake.unshift(head);   
         this.snake.pop();
-    }
-}
-
+    }}
 class Board {
-    
-}
+    constructor(width, height, squareSize){
+        ctx.fillStyle = 'white';
+        ctx.strokeStyle = 'darkblue';
+        ctx.fillRect(0, 0, width, height);
+        ctx.strokeRect(0, 0, width, height);
+    }}
  
+// main();
+let board = new Board(400, 400, 1);
+
+// function main() {
+//     setTimeout(function onTick() {
+//         Board.clear_board();
+//         Snake.move_snake();
+//         Snake.drawSnake(); 
+//         main();
+//     }, 100)}
 
 
-const board_border = 'black';
-const board_background = "white";
-const snake_col = 'lightblue';
-const snake_border = 'darkblue';
 
-let snake = [
-  { x: 200, y: 200 },
-  { x: 190, y: 200 },
-  { x: 180, y: 200 },
-  { x: 170, y: 200 },
-  { x: 160, y: 200 }
-]
-
-let dx = 10, dy = 0;
-
-const snakeboard = document.getElementById("snakeboard");
-const snakeboard_ctx = snakeboard.getContext("2d");
-
-main();
-
-function main() {
-    setTimeout(function onTick() {
-        clear_board();
-        move_snake();
-        drawSnake();
-        main();
-    }, 100)
-}
-
-function clear_board() {
-    snakeboard_ctx.fillStyle = board_background;
-    snakeboard_ctx.strokestyle = board_border;
-    snakeboard_ctx.fillRect(0, 0, snakeboard.width, snakeboard.height);
-    snakeboard_ctx.strokeRect(0, 0, snakeboard.width, snakeboard.height);
-}  
-
-/*Function that prints the parts*/
-function drawSnake() {
-    snake.forEach(snakePart => {
-        snakeboard_ctx.fillStyle = 'lightblue';
-        snakeboard_ctx.strokeStyle = 'darkblue';
-        snakeboard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
-        snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
-    });
-}
-
-function move_snake() {
-    // Create the new Snake's head
-    const head = { x: snake[0].x + dx, y: snake[0].y + dy };
-    // Add the new head to the beginning of snake body
-    snake.unshift(head);
-    snake.pop();
-}
 
 
