d6d3b023bbbb47e3dce1738d375ec9899db2a094
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-09-02 15:05:09 -0300

    move functions

--- 3/script.js
+++ 3/script.js
@@ -1,8 +1,34 @@
 class Snake {
+    snakeboard = document.getElementById("snakeboard");
+    snakeboard_ctx = snakeboard.getContext("2d");
+    snake = [
+        { x: 200, y: 200 },
+        { x: 190, y: 200 },
+        { x: 180, y: 200 },
+        { x: 170, y: 200 },
+        { x: 160, y: 200 }
+      ]
+    constructor(){}
 
+    drawSnake() {
+        this.snake.forEach(snakePart => {
+            snakeboard_ctx.fillStyle = 'lightblue';
+            snakeboard_ctx.strokeStyle = 'darkblue';
+            snakeboard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+            snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+        });
+    }
+    
+    move_snake() {
+        // Create the new Snake's head
+        const head = { x: snake[0].x + dx, y: snake[0].y + dy };
+        // Add the new head to the beginning of snake body
+        this.snake.unshift(head);  
+        this.snake.pop();
+    }
 }
 
-class S {
+class Board {
     
 }
  
@@ -14,11 +40,11 @@ const snake_col = 'lightblue';
 const snake_border = 'darkblue';
 
 let snake = [
-    { x: 200, y: 200 },
-    { x: 190, y: 200 },
-    { x: 180, y: 200 },
-    { x: 170, y: 200 },
-    { x: 160, y: 200 }
+  { x: 200, y: 200 },
+  { x: 190, y: 200 },
+  { x: 180, y: 200 },
+  { x: 170, y: 200 },
+  { x: 160, y: 200 }
 ]
 
 let dx = 10, dy = 0;
