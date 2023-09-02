adc9a3bd17e25c526d30c04a68c0c615454690fa
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-09-02 16:22:21 -0300

    eslint fix

--- 3/script.js
+++ 3/script.js
@@ -3,19 +3,15 @@ const canvas = document.querySelector('canvas'),
 let snake = [{x: 200, y: 200}, {x: 190, y: 200}, {x: 180, y: 200}, {x: 170, y: 200}, {x: 160, y: 200}],
   changing_direction = false,
   dx = 10, dy = 0
-  
-
-
-
 
 function has_game_ended() {
   for (let i = 4; i < snake.length; i++)
-  if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
-  
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
+
   const hitLeftWall = snake[0].x < 0,
-  hitRightWall = snake[0].x > canvas.width - 10,
-  hitToptWall = snake[0].y < 0,
-  hitBottomWall = snake[0].y > canvas.height - 10
+    hitRightWall = snake[0].x > canvas.width - 10,
+    hitToptWall = snake[0].y < 0,
+    hitBottomWall = snake[0].y > canvas.height - 10
   return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall
 }
 
@@ -36,31 +32,28 @@ function change_direction(event) {
   if (keyPressed === DOWN_KEY && !goingUp) {dx = 0; dy = 10}
 }
 
-
-
 main()
 function main() {
-  if (has_game_ended()) return  
+  if (has_game_ended()) return
 
   changing_direction = false
   setTimeout(() => {
-
-      ctx.fillStyle = 'white'
+    ctx.fillStyle = 'white'
     ctx.strokeStyle = 'black'
     ctx.fillRect(0, 0, canvas.width, canvas.height)
     ctx.strokeRect(0, 0, canvas.width, canvas.height)
-  
-      const head = {x: snake[0].x + dx, y: snake[0].y + dy}
-      snake.unshift(head)
-      snake.pop()
- 
-        snake.forEach(({x, y}) => {
-          ctx.fillStyle = 'lightblue'
-          ctx.strokeStyle = 'darkblue'
-          ctx.fillRect(x, y, 10, 10) 
-          ctx.strokeRect(x, y, 10, 10)
-        })
- 
+
+    const head = {x: snake[0].x + dx, y: snake[0].y + dy}
+    snake.unshift(head)
+    snake.pop()
+
+    snake.forEach(({x, y}) => {
+      ctx.fillStyle = 'lightblue'
+      ctx.strokeStyle = 'darkblue'
+      ctx.fillRect(x, y, 10, 10)
+      ctx.strokeRect(x, y, 10, 10)
+    })
+
     main()
   }, 100)
 }
