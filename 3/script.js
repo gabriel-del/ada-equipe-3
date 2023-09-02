46eea86cbafdfce753ab2913ce1ec844cb0f1854
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-09-02 16:05:30 -0300

    rm var colors

--- 3/script.js
+++ 3/script.js
@@ -1,7 +1,3 @@
-const board_border = 'black';
-    const board_background = "white";
-    const snake_col = 'lightblue';
-    const snake_border = 'darkblue';
     
     let snake = [{x: 200, y: 200}, {x: 190, y: 200},{x: 180, y: 200}, {x: 170, y: 200}, {x: 160, y: 200} ]
 
@@ -28,16 +24,16 @@ const board_border = 'black';
       }, 100)
     }
         function clear_board() {
-      ctx.fillStyle = board_background;
-      ctx.strokestyle = board_border;
+      ctx.fillStyle = 'white';
+      ctx.strokestyle = 'black';
       ctx.fillRect(0, 0, canvas.width, canvas.height);
       ctx.strokeRect(0, 0, canvas.width, canvas.height);
     }
     
     function drawSnake() {
       snake.forEach(snakePart => {
-        ctx.fillStyle = snake_col;
-        ctx.strokestyle = snake_border;
+        ctx.fillStyle = 'lightblue';
+        ctx.strokestyle = 'darkblue';
         ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
         ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
       })
