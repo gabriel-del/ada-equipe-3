41d923bb7a5475b25f2319b0984f0bc54800cbf4
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-09-04 00:09:09 -0300

    create class Snake

--- 3/script.js
+++ 3/script.js
@@ -7,13 +7,11 @@ let snake = [{x: 200, y: 200}, {x: 190, y: 200}, {x: 180, y: 200}, {x: 170, y: 2
 function has_game_ended() {
   for (let i = 4; i < snake.length; i++)
     if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
-
     return snake[0].x < 0 || snake[0].y < 0 || snake[0].x > canvas.width - 10 || snake[0].y > canvas.height - 10
 }
 
 document.addEventListener('keydown', event => {
   const LEFT_KEY = 37, RIGHT_KEY = 39, UP_KEY = 38, DOWN_KEY = 40
-
   if (changing_direction) return
   changing_direction = true
   const keyPressed = event.keyCode,
@@ -29,14 +27,13 @@ document.addEventListener('keydown', event => {
 
 function main() {
   if (has_game_ended()) return
-  
   changing_direction = false
   setTimeout(() => {
     ctx.fillStyle = 'white'
     ctx.strokeStyle = 'black'
     ctx.fillRect(0, 0, canvas.width, canvas.height)
     ctx.strokeRect(0, 0, canvas.width, canvas.height)
-    
+
     const head = {x: snake[0].x + dx, y: snake[0].y + dy}
     snake.unshift(head)
     snake.pop()
@@ -63,3 +60,22 @@ class Board {
  }}
 
   let board = new Board(400, 400, 1);
+
+  class Snake {
+    constructor(snake){
+      this.snake = snake
+    }
+    print(){
+      snake.forEach(({x, y}) => {
+        ctx.fillStyle = 'lightblue'
+        ctx.strokeStyle = 'darkblue'
+        ctx.fillRect(x, y, 10, 10)
+        ctx.strokeRect(x, y, 10, 10)
+      })
+
+
+    }
+  }
+
+  let snake2 = new Snake(snake)
+  snake2.print()
