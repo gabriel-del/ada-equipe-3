174b44cd9147af2eebdea3852dcd670ae027ca97
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-09-07 20:01:09 -0300

    back to one snake

--- 3/script.js
+++ 3/script.js
@@ -2,10 +2,10 @@ import Game from "./Game.js"
 import Board from "./Board.js"
 import Snake from "./Snake.js"
 
-new Game(1)
-new Board(10, 50, 50)
-const snake2 = new Snake([{x: 2, y: 8}, {x: 12, y: 8}], ['a', 'w', 'd', 's'])
-const snake1 = new Snake([{x: 2, y: 5}, {x: 12, y: 5}], ['ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown'])
+new Game(5)
+new Board(20, 25, 25)
+const snake = new Snake([{x: 2, y: 5}, {x: 12, y: 5}], ['ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown'])
+// const snake2 = new Snake([{x: 2, y: 8}, {x: 12, y: 8}], ['a', 'w', 'd', 's'])
 
 
 document.addEventListener('keydown', event => {
