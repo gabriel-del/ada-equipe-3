04b4718be075118fdb543222fabd8f6164d6fef7
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-09-08 14:33:20 -0300

    hide bug on Board

--- 3/script.js
+++ 3/script.js
@@ -2,8 +2,8 @@ import Game from "./Game.js"
 import Board from "./Board.js"
 import Snake from "./Snake.js"
 
-new Game(1)
-new Board(10, 80, 50)
+new Game(6)
+new Board(10, 70, 70)
 //const snake2 = new Snake([{x: 2, y: 8}, {x: 12, y: 8}], ['a', 'w', 'd', 's'])
 const snake1 = new Snake([{x: 2, y: 5}, {x: 12, y: 5}], ['ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown'])
 
