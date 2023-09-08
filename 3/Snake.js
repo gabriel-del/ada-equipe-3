38e1353db37fa0aa3b5ba384e5f878d34bf293ea
Lucas Aquino <lucasaquinoq@gmail.com>
2023-09-07 21:21:06 -0300

    Adicionado a função da cobra não poder voltar na direção contraria a que está se movimentnado

--- 3/Snake.js
+++ 3/Snake.js
@@ -5,6 +5,7 @@ export default class Snake {
   static directions = [{x: -1, y: 0}, {x: 0, y: -1}, {x: +1, y: 0}, {x: 0, y: +1}]
   constructor(scalesInterval, keys) {
     this.scales = scalesInterval
+    this.excludeKey = 'ArrowLeft'
     if (scalesInterval[0].y === scalesInterval[1].y)
       while (this.scales[1].x - 1 !== this.scales[0].x) this.scales.splice(1, 0, {x: this.scales[1].x - 1, y: scalesInterval[0].y})
     else if (scalesInterval[0].x === scalesInterval[1].x)
@@ -12,7 +13,17 @@ export default class Snake {
     else throw new Error('X ou Y devem ser iguais')
     Board.paint(this.scales, true)
     document.addEventListener('keydown', event => {
-      keys.forEach( (key,i) => { if (event.key === key) this.direction = Snake.directions[i]})})
+      keys.forEach( (key,i) => { 
+        if (event.key === key && event.key !== this.excludeKey) this.direction = Snake.directions[i]
+      })
+      if(event.code !== this.excludeKey) {
+        if(event.code === 'ArrowLeft')  this.excludeKey = 'ArrowRight'
+        if(event.code === 'ArrowRight') this.excludeKey = 'ArrowLeft'
+        if(event.code === 'ArrowUp') this.excludeKey = 'ArrowDown'
+        if(event.code === 'ArrowDown') this.excludeKey = 'ArrowUp'
+        console.log(this.excludeKey)
+      }
+    })
     Game.snakes.push(this)
     }
   died() {console.log(`Snake ${Game.snakes.indexOf(this)} died!`) }
