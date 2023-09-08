085790a79e4fb7f5dbdd54d17f6e44e5aa3afeaf
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-09-08 17:27:21 -0300

    Game.borders working

--- 3/Snake.js
+++ 3/Snake.js
@@ -26,7 +26,12 @@ export default class Snake {
     const head = {x: this.scales.slice(-1)[0].x+this.direction.x, y: this.scales.slice(-1)[0].y+this.direction.y}
     if (Game.snakes.reduce( (acc,snake) => acc.concat(snake.scales), []).
       some(({x,y}) => x == head.x && y == head.y)) this.alive = false
-    if (Board.width - 1 < head.x || head.x <= 0 -1 || Board.height - 1 < head.y || head.y <= 0 -1) this.alive = false
+    if (Game.borders){
+      if (Board.width - 1 < head.x || head.x <= 0 -1 || Board.height - 1 < head.y || head.y <= 0 -1) this.alive = false
+    } else {
+      head.x = (head.x + Board.width) % Board.width
+      head.y = (head.y + Board.height) % Board.height
+    }
     if (this.alive) {
       this.scales.push(head)
       Board.paint([head], 'Snake')
--- 3/script.js
+++ 3/script.js
@@ -2,7 +2,7 @@ import Game from "./Game.js"
 import Board from "./Board.js"
 import Snake from "./Snake.js"
 
-new Game(6, true)
+new Game(6, false)
 new Board(30, 20, 20)
 const snake1 = new Snake([{x: 2, y: 5}, {x: 10, y: 5}], ['ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown'])
 // const snake2 = new Snake([{x: 2, y: 8}, {x: 5, y: 8}], ['a', 'w', 'd', 's'])
