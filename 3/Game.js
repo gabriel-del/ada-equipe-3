bf2b0a29a31ec273d034d283312d4929fc6feb22
Bruno Cesar <83827977+brunoo85@users.noreply.github.com>
2023-09-09 16:04:18 -0300

    localstorage com variavel score

--- 3/Game.js
+++ 3/Game.js
@@ -23,12 +23,26 @@ export default class Game {
         this.snakes.forEach(snake => {if (snake.alive) snake.move() })
       } else {this.stop() ; this.end()}
     }, Math.floor(1000 / this.speed[0]))
+    Game.snakes.forEach(snake => {
+      snake.lengthStart = snake.scales.length
+      snake.score=0
+    });
   }
 
   static stop() {clearInterval(this.#interval) ;this.running = false}
-  static end() {this.stop() ;console.log('Fim de Jogo!')}
+  static end() {this.stop() ;console.log('Fim de Jogo!')
+  Game.snakes.forEach(snake => {
+    // localStorage.setItem(`Points Snake ${Game.snakes.indexOf(snake)}`,snake.scales.length-snake.lengthStart)
+    localStorage.setItem(`Points Snake ${Game.snakes.indexOf(snake)}`,snake.score)
+    console.log(snake)
+  });
+}
   static setApple(){
-    do {Game.apple = { x: Math.floor(Math.random() * Board.width), y:  Math.floor(Math.random() * Board.height) }
+    do {Game.apple = { x: Math.floor(Math.random() * Board.width), y:  Math.floor(Math.random() * Board.height)}
+      Game.snakes.forEach(snake => {
+        snake.score++
+      })
+    
     } while (Game.snakes.reduce( (acc,snake) => acc.concat(snake.scales), []).some(({x,y}) => x == Game.apple.x && y == Game.apple.y))
     Board.paint([Game.apple], 'Apple')
   }
--- 3/script.js
+++ 3/script.js
@@ -8,9 +8,9 @@ new Game([6, 2, 1], false, false, 15)
 new Board(30, 20, 20)
 // ([snake Beginning, snake End], [Left, Up, Right, Down])
 const snake1 = new Snake([{x: 2, y: 0}, {x: 4, y: 0}], ['ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown'])
-// const snake2 = new Snake([{x: 2, y: 4}, {x: 10, y: 4}], ['a', 'w', 'd', 's'])
+//  const snake2 = new Snake([{x: 2, y: 4}, {x: 10, y: 4}], ['a', 'w', 'd', 's'])
 // const snake3 = new Snake([{x: 2, y: 8}, {x: 10, y: 8}], ['a', 'w', 'd', 's'])
-// const snake4 = new Snake([{x: 2, y: 12}, {x: 10, y: 12}], ['a', 'w', 'd', 's'])
+//  const snake4 = new Snake([{x: 2, y: 12}, {x: 10, y: 12}], ['a', 'w', 'd', 's'])
 
 
 document.addEventListener('keydown', event => {
