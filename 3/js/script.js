b7f422e7cbdb0c9914d7bf56c2c3a93a490ffe99
Bruno Cesar <83827977+brunoo85@users.noreply.github.com>
2023-09-12 19:33:14 -0300

    #47 o usuario já escolhe o speed e a meta de pontos

--- 3/js/script.js
+++ 3/js/script.js
@@ -10,7 +10,7 @@ new Board(40, 20, 20)
 new Snake([{x: 1, y: 10}, {x: 5, y: 10}], ['ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown'])
 // new Snake([{x: 1, y:15}, {x: 5, y: 15}], ['a', 'w', 'd', 's'])
 // new Snake([{x: 2, y: 8}, {x: 10, y: 8}], ['a', 'w', 'd', 's'])
-// new Snake([{x: 2, y: 12}, {x: 10, y: 12}], ['a', 'w', 'd', 's'])
+//new Snake([{x: 2, y: 12}, {x: 10, y: 12}], ['a', 'w', 'd', 's'])
 
 // document.addEventListener('keydown', event => {
 //   if (event.code && Game.running && Game.paused) Game.start()
@@ -26,18 +26,38 @@ const option = document.getElementsByName("gameMode");
 
 let optionGame = "";
 
-          for (const opcao of option) {
-               if (opcao.checked) {
-                optionGame = opcao.value;
-               break;
-               }
-          }
+for (let opcao of option) {
+  if (opcao.checked) {
+    optionGame = opcao.value;
+    break;
+  }
+}
 
-buttonStart.addEventListener("click", event => {
-  if (event.isTrusted) new Game([6, 1, 1], true, true, 999)
+let form = document.getElementById("form")
+console.log(form)
+
+form.addEventListener("submit", function(event){
+
+  event.preventDefault()
+
+  // if(speed & widthTab & heightTab & goalPoints & optionGame){
+    new Game([speed.value, 1, 1], true, true, goalPoints.value)
+  //   new Board(40,widthTab.value , heightTab.value)
+  //   new Snake([{x: 1, y: 10}, {x: 5, y: 10}], ['ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown'])
+  // }
+  
   if(Game.running && Game.paused) Game.start()
   // if (event.key === 'q') Game.stop()
 })
+
+
+
+
+// buttonStart.addEventListener("click", event => {
+  
+//   if(Game.running && Game.paused) Game.start()
+//   // if (event.key === 'q') Game.stop()
+// })
 
 document.addEventListener('keydown', event => {
     // if(Game.running && Game.paused) Game.start()
